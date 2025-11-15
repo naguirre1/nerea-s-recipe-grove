8a7153b4f107b690953ca6b5c8b32bdfe4c3339c
naguirre1 <nereaguirre1990@gmail.com>
2025-11-15 21:28:33 +0100

    Refactor to use ES6 import syntax

--- convert-recipes.js
+++ convert-recipes.js
@@ -1,5 +1,8 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 function convertRecipe(filePath) {
   const content = fs.readFileSync(filePath, 'utf8');
